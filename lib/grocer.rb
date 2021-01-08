79dccf120774ec0e0076d7fab1af87d9613499ba
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-08 15:53:51 -0500

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,16 +1,34 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  
+  collection.each do |cosa|   # does a check for each 'cosa' (item)
+    if cosa[:item] == name
+        return cosa
+    end
+  end
+
+  return nil
 
 end
 
-def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
+def consolidate_cart(cart)
+  receipt = []
+
+  cart.each do |cosa|
+    alreadyscanned = false          #default unless the next check finds otherwise
+    receipt.each do |thing|         #check each item on receipt to see if its already there
+      if thing[:item]==cosa[:item]
+        thing[:count] += 1
+        alreadyscanned = true
+      end
+    end
+    if !alreadyscanned              #add item to receipt and initialize its count to 1
+      receipt.push(cosa)
+      receipt[receipt.index(cosa)][:count] =1
+    end
+  end
+ 
+  receipt
 end
 
 
